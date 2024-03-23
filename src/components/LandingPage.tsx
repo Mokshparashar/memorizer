80a6149493a8488e1bd7c26b021071529833d1f0
adarsharma02 <you@example.com>
2024-03-23 13:39:16 +0530

    .

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,12 @@
+import Landing from "./Landing";
+import Navbar from "./Navbar";
+
+function LandingPage() {
+  return (
+    <>
+      <Navbar /> <Landing />{" "}
+    </>
+  );
+}
+
+export default LandingPage;
