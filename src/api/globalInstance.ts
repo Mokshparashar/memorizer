ff35cdd3dd506acdf54e4612c7720cfaa63d8248
Moksh parashar <parasharm239@gmail.com>
2024-04-02 05:45:09 -0700

    typo fixed

--- src/api/globalInstance.ts
+++ src/api/globalInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const globalInstance = axios.create({
-  // baseURL: "https://memorizer-backbone.onrender.com",
-  baseURL: "http://localhost:8000",
+  baseURL: "https://memorizer-backbone.onrender.com",
+  // baseURL: "http://localhost:8000",
 
   headers: {
     "Content-Type": "Application/json",
