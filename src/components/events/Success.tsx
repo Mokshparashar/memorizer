f3c89dbbe747f3bf86d605a767fdc93a9019ea36
Moksh parashar <parasharm239@gmail.com>
2024-05-05 18:56:52 -0700

    .

--- src/components/events/Success.tsx
+++ src/components/events/Success.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Success(props: EventModalPropInterface) {
   return (
     <div className="w-full h-14 bg-green-500 absolute top-0 z-50 text-white flex items-center justify-center">
